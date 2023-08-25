262138e381e934b3687a6503a5c9c4c70d835398
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-08-26 01:34:48 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6257940664:AAFVnJUI5UcDC7mym3bwzb8a5h3GI1kFHBw")
-API_ID = int(os.environ.get("API_ID", "25982300"))
-API_HASH = os.environ.get("API_HASH", "7ef41890f2707de100466b758eff9153")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001878999657"))
-AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "5657257558").split())
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6697713258:AAFDfdivj4INPJVDlfI_k7APbYxds4wmGbs")
+API_ID = int(os.environ.get("API_ID", "22825629"))
+API_HASH = os.environ.get("API_HASH", "e8db542482a1638b4e5b03ed1ddae521")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001743550303"))
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "6404342282").split())
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://Luffy:Malik10_@cluster0.f0cpndf.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "BroadcastBot")
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
